b842100a03ce3853e080ff709a10600d217d0a40
Bencece <zahoranbence@hotmail.com>
2021-04-27 10:02:12 +0200

    Create the Android project

--- ShipLoadingProblem/src/app/app.module.ts
+++ ShipLoadingProblem/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IntroComponent } from './intro/intro.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IntroComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, CommonModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- ShipLoadingProblem/src/app/intro/intro.component.ts
+++ ShipLoadingProblem/src/app/intro/intro.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import Ship from '../models/ship';
 import Container from '../models/container';
+
+
 
 @Component({
   selector: 'app-intro',
@@ -8,17 +10,20 @@ import Container from '../models/container';
   styleUrls: ['./intro.component.scss'],
 })
 
+
 export class IntroComponent implements OnInit {
 
   constructor() {}
 
   ngOnInit() {
-    let testShip = new Ship();
-    let c1 = new Container();
-    let c2 = new Container(50, 100, 800);
-    let c3 = new Container(60, 80, 450);
-    var testDock = {c1, c2, c3}
   }
+  
+  testShip = new Ship();
+  c1 = new Container();
+  c2 = new Container(50, 100, 800);
+  c3 = new Container(60, 80, 450);
+  testDock = [this.c1, this.c2, this.c3]
 
+  teszt ="teszt"
 
 }
